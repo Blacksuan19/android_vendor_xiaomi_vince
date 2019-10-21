e5b8cf5c62781330dbf8516b6d48adfb796d789a
Blacksuan19 <Abubakaryagob@gmail.com>
2019-10-21 18:24:44 +0800

    msm8953-common: add missing libbtconfigstore
    kanged from https://github.com/dopaemon/vendor_xiaomi_whyred/commit/32c6511d4dedfde4654a4991ca492894e7ac3a59
    
    Signed-off-by: Blacksuan19 <Abubakaryagob@gmail.com>

--- msm8953-common/Android.mk
+++ msm8953-common/Android.mk
@@ -171,4 +171,14 @@ LOCAL_MODULE_SUFFIX := .apk
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libbtconfigstore
+LOCAL_MODULE_OWNER := xiaomi
+LOCAL_SRC_FILES_64 := proprietary/lib64/libbtconfigstore.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libbtconfigstore.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
 endif
